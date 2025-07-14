a2039e81069c6801da9c6e287ba5bf7555d875b6
AKRP_ <NubXD2551@gmail.com>
2025-07-14 10:10:10 +0700

    image

--- NSC Project 2025/src/data/server.js
+++ NSC Project 2025/src/data/server.js
@@ -1,36 +1,12 @@
-const express = require("express");
-const mysql = require("mysql2");
-const dotenv = require("dotenv");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+import { initializeApp } from "firebase/app";
 
-const app = express();
-dotenv.config();
+const firebaseConfig = {
+  apiKey: "AIzaSyA66w2jbm4ySC_a8frCPxdhUYxLBmlgSpo",
+  authDomain: "nsc-server.firebaseapp.com",
+  projectId: "nsc-server",
+  storageBucket: "nsc-server.firebasestorage.app",
+  messagingSenderId: "13678340649",
+  appId: "1:13678340649:web:b4fb89bbb1b1810506ca38",
+};
 
-const port = process.env.PORT || 3000;
-
-app.use(bodyParser.json());
-app.use(cors());
-
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
-});
-
-db.connect((err) => {
-  if (err) {
-    throw err;
-  }
-  console.log("Connected to database");
-});
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+const app = initializeApp(firebaseConfig);
